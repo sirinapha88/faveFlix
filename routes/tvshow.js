7b82b2591b70a3474721353444d4cf0d4660985b
sirinapha88 <sirinapha888@gmail.com>
2016-01-09 13:10:45 -0800

    added edit and new.ejs files

--- routes/tvshow.js
+++ routes/tvshow.js
@@ -11,4 +11,35 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.get('/new',function(req,res){
+	res.render("tvshows/new");
+});
+
+router.post('/',function(req,res){
+	knex('tvshows').insert(req.body).then(function(){
+		res.redirect('/tvshows');
+	});
+});
+
+router.get('/:id/edit',function(req,res){
+	var id = req.params.id;
+	knex('tvshows').where({id:id}).first().then(function(author){
+		res.render("tvshows/edit", {author:author});
+	});
+});
+
+router.put('/:id',function(req,res){
+	var id = req.params.id;
+	knex('tvshows').where({id:id}).first().update(req.body).then(function(){
+		res.redirect('/tvshows');
+	});
+});
+
+router.delete('/:id',function(req,res){
+	var id = req.params.id;
+	knex('tvshows').where({id:id}).del().then(function(){
+		res.redirect('/tvshows');
+	});
+});
+
 module.exports = router;
