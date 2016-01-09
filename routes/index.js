f8f138e61f10609bf26df94a5fea8b6eba8d5dd7
sirinapha88 <sirinapha888@gmail.com>
2016-01-08 16:55:12 -0800

    changed ejs files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var MovieDB = require('moviedb')('782b6c90018378ce662350a3bc5cdc63');
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var name;
+	MovieDB.miscPopularTvs(function(err, searchRes){
+		topRates = searchRes.results;
+  		res.render('index', {topRates:topRates});
+	});
 });
 
 module.exports = router;
--- routes/tvshow.js
+++ routes/tvshow.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var MovieDB = require('moviedb')('782b6c90018378ce662350a3bc5cdc63');
-// var theMovieDb = require('./public/javascripts/theMovieDb');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
