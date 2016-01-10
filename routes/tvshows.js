534e4f8d91dfaa5f87846cfb153926b6f33d2d64
sirinapha88 <sirinapha888@gmail.com>
2016-01-09 17:07:09 -0800

    added display all the show and image

--- routes/tvshows.js
+++ routes/tvshows.js
@@ -9,14 +9,14 @@ router.get('/', function(req, res, next) {
   		res.render('index', {topRates:topRates});
 	});
 });
-
+// Popular page
 router.get('/popular',function(req,res){
 	MovieDB.miscPopularTvs(function(err, searchRes){
 		populars = searchRes.results;
 		res.render("tvshows/popular", {populars:populars});
 	});
 });
-
+// New Release page
 router.get('/newRelease',function(req,res){
 	MovieDB.miscPopularTvs(function(err, searchRes){
 		topRates = searchRes.results;
@@ -24,6 +24,36 @@ router.get('/newRelease',function(req,res){
 	});
 });
 
+// Search tv show
+// router.get('/search',function(req,res){	
+	
+// 	res.render("tvshows/search");
+
+// });
+
+router.get('/search',function(req,res){
+
+    var tvshowQuery = req.query.tvshow;
+    console.log("this is " + tvshowQuery);
+    res.redirect('/tvshows/search/' + tvshowQuery);
+  });
+
+
+//http://localhost:3000/search?tvshow=house
+// http://localhost:3000/search/sound%20provider
+
+router.get('/search/:searchString', function(req,res){
+	var tvshowSearch = req.params.searchString;
+	 console.log(tvshowSearch);
+	MovieDB.searchTv({query: tvshowSearch },function(err, searchRes){
+		
+		tvshowLists = searchRes.results;
+		console.log(tvshowLists)
+  		res.render('tvshows/displayShow', {tvshowLists:tvshowLists});
+	});
+});
+
+// new 
 router.get('/new',function(req,res){
 	res.render("tvshows/new");
 });
