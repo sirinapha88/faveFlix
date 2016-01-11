b10cc5876df9c7f7073374de0dae619feeb28b1e
sirinapha88 <sirinapha888@gmail.com>
2016-01-11 09:26:56 -0800

    changed routes tvshows.js

--- routes/tvshows.js
+++ routes/tvshows.js
@@ -18,19 +18,13 @@ router.get('/popular',function(req,res){
 });
 // New Release page
 router.get('/newRelease',function(req,res){
-	MovieDB.miscPopularTvs(function(err, searchRes){
+	MovieDB.miscTopRatedTvs(function(err, searchRes){
 		topRates = searchRes.results;
 		res.render("tvshows/newRelease" , {topRates:topRates});
 	});
 });
 
 // Search tv show
-// router.get('/search',function(req,res){	
-	
-// 	res.render("tvshows/search");
-
-// });
-
 router.get('/search',function(req,res){
 
     var tvshowQuery = req.query.tvshow;
@@ -38,17 +32,11 @@ router.get('/search',function(req,res){
     res.redirect('/tvshows/search/' + tvshowQuery);
   });
 
-
-//http://localhost:3000/search?tvshow=house
-// http://localhost:3000/search/sound%20provider
-
 router.get('/search/:searchString', function(req,res){
 	var tvshowSearch = req.params.searchString;
 	 console.log(tvshowSearch);
 	MovieDB.searchTv({query: tvshowSearch },function(err, searchRes){
-		
 		tvshowLists = searchRes.results;
-		console.log(tvshowLists)
   		res.render('tvshows/displayShow', {tvshowLists:tvshowLists});
 	});
 });
@@ -58,12 +46,14 @@ router.get('/new',function(req,res){
 	res.render("tvshows/new");
 });
 
+// Create
 router.post('/',function(req,res){
 	knex('tvshows').insert(req.body).then(function(){
 		res.redirect('/tvshows');
 	});
 });
 
+// Edit
 router.get('/:id/edit',function(req,res){
 	var id = req.params.id;
 	knex('tvshows').where({id:id}).first().then(function(author){
@@ -71,6 +61,7 @@ router.get('/:id/edit',function(req,res){
 	});
 });
 
+// Update
 router.put('/:id',function(req,res){
 	var id = req.params.id;
 	knex('tvshows').where({id:id}).first().update(req.body).then(function(){
@@ -78,6 +69,7 @@ router.put('/:id',function(req,res){
 	});
 });
 
+// Delete
 router.delete('/:id',function(req,res){
 	var id = req.params.id;
 	knex('tvshows').where({id:id}).del().then(function(){
